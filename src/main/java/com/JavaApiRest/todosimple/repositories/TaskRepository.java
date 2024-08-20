614d3b350db2260816655f3aa8a6428ebdc62206
Guimathis <guimathias1906@gmail.com>
2024-08-20 16:28:40 -0400

    Create User and Tasks repository

---
+++ src/main/java/com/JavaApiRest/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,22 @@
+package com.JavaApiRest.todosimple.repositories;
+
+import com.JavaApiRest.todosimple.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+        List<Task> findByUser_Id(Long id);
+
+//    @Query(value = "select t from Task t where t.user.id = :id")
+//    List<Task> findByUser_Id(@Param("id") Long id);
+
+    // @Query(value = "SELECT * FROM task t WHERE t.user_id = :id", nativeQuery =
+    // true)
+    // List<Task> findByUser_Id(@Param("id") Long id);
+}
