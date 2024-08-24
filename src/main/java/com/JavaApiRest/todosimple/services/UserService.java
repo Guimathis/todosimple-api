03582dc64b5461fff8b1eb2708d9e12155edba2f
Guimathis <guimathias1906@gmail.com>
2024-08-24 15:14:58 -0400

    Create User Service

---
+++ src/main/java/com/JavaApiRest/todosimple/services/UserService.java
@@ -0,0 +1,48 @@
+package com.JavaApiRest.todosimple.services;
+
+import com.JavaApiRest.todosimple.models.User;
+import com.JavaApiRest.todosimple.repositories.TaskRepository;
+import com.JavaApiRest.todosimple.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private TaskRepository taskRepository;
+
+    private User findById(Long id){
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException(
+                "Usuário não encontrado. Id:" + id + ", Tipo: " + User.class.getName()
+        ));
+    }
+    @Transactional //Usar anotação para operações de criação/exclusão no BD
+    public User createUser(User newUser){
+        newUser = this.userRepository.save(newUser);
+        this.taskRepository.saveAll(newUser.getTasks());//Se o usuário possuir tasks, as salva
+        return newUser;
+
+    }
+    @Transactional
+    public User userUpdate(User user) {
+        User newUser = findById(user.getId());
+        newUser.setPassword(user.getPassword());
+        return this.userRepository.save(newUser);
+    }
+
+    public void deleteUser(Long id){
+        User user = findById(id);
+        try {
+            this.userRepository.deleteById(id);
+        }catch (Exception e){
+            throw new RuntimeException("Não foi possivel deletar o Usuário " + user.getUsername() + ", pois há tarefas ligadas a ele." );
+        }
+    }
+
+}
