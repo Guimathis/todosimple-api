299b1ec539a4fb8eb73fa89f00fc6f182fedc5b6
Guimathis <guimathias1906@gmail.com>
2024-09-30 18:16:34 -0400

    Docker version

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-# install docker
+install docker
 sudo apt -y update
 sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -10,7 +10,7 @@ sudo chmod 666 /var/run/docker.sock
 docker ps
 
 # install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/4.34.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
