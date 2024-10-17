7c9c0d3d6beb52ce2580e7c02b0b7b443cde9158
guimathis <guimathias1906@gmail.com>
2024-10-16 22:02:16 -0400

    Added Task creation

--- view/scripts/task.js
+++ view/scripts/task.js
@@ -1,30 +1,36 @@
 async function createtask() {
-  let description = document.getElementById("description").value;
+    let description = document.getElementById("description").value;
+    let key = "Authorization";
+    const response = await fetch("http://localhost:8080/task", {
 
-  console.log(description);
+        method: "POST",
+        headers: new Headers({
+            "Content-Type": "application/json; charset=utf8",
+            Accept: "application/json",
+            // Adiciona o token diretamente como está no localStorage
+            Authorization: localStorage.getItem(key),
+        }),
+        body: JSON.stringify({
+            description: description,
+        }),
+    });
 
-  const response = await fetch("http://localhost:8080/task", {
-    method: "POST",
-    headers: new Headers({
-      "Content-Type": "application/json; charset=utf8",
-      Accept: "application/json",
-    }),
-    body: JSON.stringify({
-      description: description,
-    }),
-  });
 
-  if (response.ok) {
-    showToast("#okToast");
-  } else {
-    showToast("#errorToast");
-  }
-}
+    if (response.ok) {
+        showToast("#okToast");
+    } else {
+        showToast("#errorToast");
+    }
 
-function showToast(id) {
-  var toastElList = [].slice.call(document.querySelectorAll(id));
-  var toastList = toastElList.map(function (toastEl) {
-    return new bootstrap.Toast(toastEl);
-  });
-  toastList.forEach((toast) => toast.show());
+    window.setTimeout(function () {
+        window.location = "index.html";
+    }, 2000);
+
+    function showToast(id) {
+        var toastElList = [].slice.call(document.querySelectorAll(id));
+        var toastList = toastElList.map(function (toastEl) {
+            return new bootstrap.Toast(toastEl);
+        });
+        toastList.forEach((toast) => toast.show());
+    }
 }
